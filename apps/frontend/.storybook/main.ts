f92067de50005b8f6721a40c24aad8e51bec915e
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2025-10-23 18:25:58 +0530

    optimized

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -2,14 +2,10 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-onboarding",
   ],
   framework: {
     name: "@storybook/nextjs-vite",
